d8369c49d781daa81fa71128906b2066f0c2e4ce
Christian Beier <dontmind@freeshell.org>
2017-11-27 09:52:36 +0100

    emr3k: try one big fw part for mtd

--- target/linux/ar71xx/image/senao.mk
+++ target/linux/ar71xx/image/senao.mk
@@ -34,11 +34,10 @@ define Device/emr3000
   DEVICE_TITLE := EnGenius EMR3000
   BOARDNAME := EMR3000
   DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-storage kmod-bluetooth kmod-ath10k ath10k-firmware-qca988x
-  KERNEL_SIZE := 1408k
   IMAGE_SIZE := 32064k
   IMAGES += factory.bin
-  MTDPARTS := spi0.0:256k(u-boot)ro,64k(u-boot-env)ro,64k(art)ro,1408k(kernel),30656k(rootfs),320k(config)ro,32064k@0x60000(firmware)
+  MTDPARTS := spi0.0:256k(u-boot)ro,64k(u-boot-env)ro,64k(art)ro,32064k(firmware),320k(config)ro
   IMAGE/factory.bin/squashfs := append-rootfs | pad-rootfs | senao-factory-image emr3000 $$$$@
-  IMAGE/sysupgrade.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs | pad-rootfs | check-size $$$$(IMAGE_SIZE)
 endef
 TARGET_DEVICES += emr3000
+
